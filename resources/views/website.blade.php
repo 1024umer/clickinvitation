da4095fa95c3c924300a713f797ce5df14b7d581
Muhammad Umer <127780703+1024umer@users.noreply.github.com>
2024-02-28 14:23:22 -0700

    fixes

--- resources/views/website.blade.php
+++ resources/views/website.blade.php
@@ -12,10 +12,10 @@
         rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-    @php
+    <link rel="stylesheet" href="https://searchmarketingservices.online/fonts/index.css">
+    {{-- @php
         include_once public_path('templates/fontsLink.php');
-    @endphp
+    @endphp --}}
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
