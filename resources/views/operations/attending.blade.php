c7e0c6607a2b5a1d7073da78100a82ab25f9d93a
Hammad <hw13604@gmail.com>
2024-03-05 00:16:07 +0500

    fix

--- resources/views/operations/attending.blade.php
+++ resources/views/operations/attending.blade.php
@@ -159,7 +159,7 @@
                                         <button style="width: 100%" ng-if="member.opened == 2"
                                             class="btn btn-success btn-sm" disabled>{{ __('attending.CONFIRM') }}</button>
                                         <button style="width: 100%" id="confirm" ng-if="member.opened == null || member.opened == 1"
-                                            class="btn btn-success btn-sm" ng-click="confirmGuest()" name="guest_id" value="member.id_guest">{{ __('attending.CONFIRM') }}</button>
+                                            class="btn btn-success btn-sm" ng-click="confirmGuest()" name="guest_id" value="${member.id_guest}">{{ __('attending.CONFIRM') }}</button>
                                     @if ($isCorporate)
                                         <button class="btn btn-success" data-bs-toggle="modal"
                                             data-bs-target="#seatguestModal"
