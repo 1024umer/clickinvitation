509f3de563bdcd4f8945de98be1742b91dd4c760
Hammad <hw13604@gmail.com>
2024-03-05 02:54:59 +0500

    dd

--- resources/views/operations/addphotos.blade.php
+++ resources/views/operations/addphotos.blade.php
@@ -214,12 +214,11 @@
 
                 $scope.imageUpload = function(event) {
                     var files = event.target.files; //FileList object
-                    console.log(files);
                     for (var i = 0; i < files.length; i++) {
                         var file = files[i];
                         var reader = new FileReader();
                         reader.onload = $scope.imageIsLoaded;
-                        reader.readAsDataURL(file);
+                        // reader.readAsDataURL(file);
                     }
                     $scope.saveyes = 1;
                 }
