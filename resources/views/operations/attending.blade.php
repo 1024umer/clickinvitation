72792654eeaba7b3fc3caf2270cd991f359cbfd1
Hammad <hw13604@gmail.com>
2024-03-04 23:52:17 +0500

    Push

--- resources/views/operations/attending.blade.php
+++ resources/views/operations/attending.blade.php
@@ -99,7 +99,7 @@
                                         class="btn btn-success btn-sm" disabled>{{ __('attending.CONFIRM') }}</button>
                                     @else
                                     <button style="width: 100%" id="confirm"
-                                        class="btn btn-success btn-sm" onclick="confirmGuest()" name="guest_id" value="{{ $guest->id_guest }}">{{ __('attending.CONFIRM') }}</button>
+                                        class="btn btn-success btn-sm" ng-click="confirmGuest()" name="guest_id" value="{{ $guest->id_guest }}">{{ __('attending.CONFIRM') }}</button>
                                     @endif
                                 </div>
                             </div>
@@ -537,22 +537,6 @@
     </section>
 
     <script>
-        function confirmGuest() {
-                    $.ajax({
-                        type: "POST",
-                        url: "/confirm-guest",
-                        data: {
-                            idevent: {{ $group->id_event }},
-                            idguest: {{ $group->id_guest }},
-                        },
-                        success: function(data) {
-                            console.log(data);
-                        },
-                        error: function(data) {
-                            console.log(data);
-                        }
-                    })
-                }
         var sampleApp = angular.module('sampleApp', ['ngRoute', 'ngAnimate', 'ui.sortable', 'ngImgCrop']);
         sampleApp.controller('AttendingCtrl', ['$scope', '$route', '$http', '$location', '$routeParams', '$window',
             '$interval',
@@ -646,18 +630,18 @@
                 };
                 $scope.getTables();
 
-                // $scope.confirmGuest = function() {
-                //     $http({
-                //         method: 'POST',
-                //         url: '/confirm-guest',
-                //         data: {
-                //             idevent: {{ $group->id_event }},
-                //             idguest: {{ $group->id_guest }},
-                //         },
-                //     }).then(function(response) {
-                //         console.log(response);
-                //     });
-                // };
+                $scope.confirmGuest = function() {
+                    $http({
+                        method: 'POST',
+                        url: '/confirm-guest',
+                        data: {
+                            idevent: {{ $group->id_event }},
+                            idguest: {{ $group->id_guest }},
+                        },
+                    }).then(function(response) {
+                        console.log(response);
+                    });
+                };
 
 
 
