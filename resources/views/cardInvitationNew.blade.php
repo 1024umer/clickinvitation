5de9d09fcafb7578fec79ce711452a3995020079
Hammad <hw13604@gmail.com>
2024-03-07 21:27:59 +0500

    Fonts issue Solved !!

--- resources/views/cardInvitationNew.blade.php
+++ resources/views/cardInvitationNew.blade.php
@@ -18,6 +18,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/fontstyle.css">
+    <link rel="stylesheet" href="https://searchmarketingservices.online/fonts/index.css">
 </head>
 <div class="thisBody" style="display:none">
 
