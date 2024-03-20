e90ead6bca95122941edafa84c624ab686a80bce
Hammad <hw13604@gmail.com>
2024-03-20 22:30:04 +0500

    Fonts Link in Preview!!

--- resources/views/anniversary_animationPreview.blade.php
+++ resources/views/anniversary_animationPreview.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://searchmarketingservices.online/fonts/index.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.2/dist/confetti.browser.min.js"></script>
 </head>
