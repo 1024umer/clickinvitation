96d6ddb01003d7bd6aecd23cdd03ccd960053345
Hammad <hw13604@gmail.com>
2024-04-23 03:26:30 +0500

    Title..

--- resources/views/auth/success.blade.php
+++ resources/views/auth/success.blade.php
@@ -17,6 +17,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="title" content="Congratulations! You're Now Registered | Click Invitation">
+    <meta name="description" content="Congratulations! You're officially registered with Click Invitation. Get ready to explore exciting features and connect with like-minded individuals.">
 
     <title>Click Invitation - Success</title>
 
